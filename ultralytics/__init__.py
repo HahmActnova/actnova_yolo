c498343d9dc9f35c62b66dac3bc32fc573588f4e
HahmActnova <hahmdong@actnova.io>
2023-07-25 17:35:55 +0900

    add ActnovaModel

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -1,6 +1,6 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 
 from ultralytics.engine.model import YOLO
 from ultralytics.hub import start
--- ultralytics/models/yolo/__init__.py
+++ ultralytics/models/yolo/__init__.py
@@ -1,5 +1,5 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-from ultralytics.models.yolo import classify, detect, pose, segment
+from ultralytics.models.yolo import classify, detect, pose, segment, actnova
 
-__all__ = 'classify', 'segment', 'detect', 'pose'
+__all__ = 'classify', 'segment', 'detect', 'pose', 'actnova'
