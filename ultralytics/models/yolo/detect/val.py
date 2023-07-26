f67ae34b190dd066d296e3f29db42ade67db808c
HahmActnova <hahmdong@actnova.io>
2023-07-26 17:35:38 +0900

    add RMSE

--- ultralytics/models/yolo/detect/val.py
+++ ultralytics/models/yolo/detect/val.py
@@ -125,7 +125,7 @@ class DetectionValidator(BaseValidator):
         stats = [torch.cat(x, 0).cpu().numpy() for x in zip(*self.stats)]  # to numpy
         if len(stats) and stats[0].any():
             self.metrics.process(*stats)
-        self.nt_per_class = np.bincount(stats[-1].astype(int), minlength=self.nc)  # number of targets per class
+        self.nt_per_class = np.bincount(stats[-3].astype(int), minlength=self.nc)  # number of targets per class
         return self.metrics.results_dict
 
     def print_results(self):
